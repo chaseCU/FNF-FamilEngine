6393e304c53a48b8e209d7159d18e0db6d19e280
family1242 <63172658+family1242@users.noreply.github.com>
2021-11-17 15:12:36 -0600

    junk

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -84,7 +84,7 @@ class TitleState extends MusicBeatState
 		#if CHECK_FOR_UPDATES
 		if(!closedState) {
 			trace('checking for update');
-			var http = new haxe.Http("https://raw.githubusercontent.com/ShadowMario/FNF-PsychEngine/main/gitVersion.txt");
+			var http = new haxe.Http("https://raw.githubusercontent.com/family1242/FNF-FamilEngine/main/gitVersion.txt");
 			
 			http.onData = function (data:String)
 			{
@@ -374,7 +374,11 @@ class TitleState extends MusicBeatState
 
 				new FlxTimer().start(1, function(tmr:FlxTimer)
 				{
-					MusicBeatState.switchState(new MainMenuState());
+					if (mustUpdate) {
+						MusicBeatState.switchState(new OutdatedState());
+					} else {
+						MusicBeatState.switchState(new MainMenuState());
+					}
 					closedState = true;
 				});
 				// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
